31fb1284a0e05e3a55b15ec1ce9a522db79d9dbe
Hedi Boussada <hboussada-ext@ALMERYS.local>
2020-09-17 17:54:02 +0100

    Modif

--- src/app/list-conges/list-conges.component.ts
+++ src/app/list-conges/list-conges.component.ts
@@ -8,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class ListCongesComponent implements OnInit {
 conges = [];
   loading = false;
+  currentUserString = localStorage.getItem('user');
+  currentUser = JSON.parse(this.currentUserString);
+  isEmploye = false;
+  isRH = true;
+  isAdmin = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.isEmploye = this.currentUser.role === 'EMPLOYE';
+    this.isRH = this.currentUser.role === 'RH';
+    this.isAdmin = this.currentUser.role === 'ADMIN';
     this.conges = [
       {employe: 'Rim Hajji', demandeCongeType: 'Maladie', motif: 'Maladie', dateDeb: '02-09-2020', dateFin: '04-09-2020', nbrJrs: '2',
         remplacant: 'Asya Javayant', tacheDele: 'Migration vers le nouveau platform', dateDem: '02-09-2020', status: 'VALIDATED' },
--- src/app/liste-avance-salaire/liste-avance-salaire.component.ts
+++ src/app/liste-avance-salaire/liste-avance-salaire.component.ts
@@ -8,11 +8,18 @@ import {Component, OnInit} from '@angular/core';
 export class ListeAvanceSalaireComponent implements OnInit {
   demandes = [];
   loading = false;
-
+  currentUserString = localStorage.getItem('user');
+  currentUser = JSON.parse(this.currentUserString);
+  isEmploye = false;
+  isRH = true;
+  isAdmin = false;
   constructor() {
   }
 
   ngOnInit(): void {
+    this.isEmploye = this.currentUser.role === 'EMPLOYE';
+    this.isRH = this.currentUser.role === 'RH';
+    this.isAdmin = this.currentUser.role === 'ADMIN';
     this.demandes = [
       {employe: 'Rim Hajji', montant: '150', dateDem: '02-09-2020', status: 'IN_PROGRESS'},
       {employe: 'Amy Elsner', montant: '800', dateDem: '02-09-2020', status: 'IN_PROGRESS'},
